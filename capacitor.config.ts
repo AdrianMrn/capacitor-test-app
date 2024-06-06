edab9f8ab5bafbab44f9583bfe6c7c5a74a36648
Adriaan Marain <adriaanmarain300@gmail.com>
2024-06-06 12:27:51 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Capacitor Test App',
+  webDir: 'dist'
+};
+
+export default config;
